ed23655618b27029c642acf1e57c735e78565d9b
XDfelek <feleksteam@wp.pl>
2021-03-03 18:37:50 +0100

    GPS 1.01

--- src/main/java/com/example/demo/ImageDataExtractor.java
+++ src/main/java/com/example/demo/ImageDataExtractor.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 public class ImageDataExtractor {
     private final Logger logger = LoggerFactory.getLogger(MapitApplication.class);
 
-    public String imagePathTestWithGPS = "D:\\GIT Repozytorium\\MapIt Project\\src\\main\\resources\\photos\\IMG_20190224_155957.jpg";
-    public String imagePathTestWithoutGPS = "D:\\GIT Repozytorium\\MapIt Project\\src\\main\\resources\\photos\\15380582_377815685889730_2247397068673265016_n.jpg";
+    public String imagePathTestWithGPS = "src/main/resources/photos/IMG_20190224_155957.jpg";
+    public String imagePathTestWithoutGPS = "src/main/resources/photos/15380582_377815685889730_2247397068673265016_n.jpg";
 
     public double[] getImageGPS(String imagePath){
         javaxt.io.Image image = new javaxt.io.Image(imagePath);
